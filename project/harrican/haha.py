f1fa41a39913528d1335e6e47452cf0053674d12
Jing <qinj1994@163.com>
2018-04-19 06:01:05 +0200

    Add files via upload

---
+++ project/harrican/haha.py
@@ -0,0 +1,9 @@
+import io
+from urllib import request
+import csv
+
+response = request.urlopen('https://dq-content.s3.amazonaws.com/251/storm_data.csv')
+reader = csv.reader(io.textIOWrapper(response))
+
+for line in reader:
+    print(line)
